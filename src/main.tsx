d433f257647217d6b45c82fb88e5631505945fb9
bishoe01 <bishoe01@gmail.com>
2023-09-16 19:28:06 +0900

    [Chore] 오류 나는 부분 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,20 @@
-import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Home from './pages/Home.tsx';
-import LoginPage from './pages/LoginPage.tsx';
-import Order from './pages/Order.tsx';
-import FoodDetail from './pages/FoodDetail.tsx';
-import TmpDetail from './pages/TmpDetail.tsx';
-import FakeDetail from './pages/FakeDetail.tsx';
-import OrderList from './pages/OrderList.tsx';
-import FakeOrder from './pages/FakeOrder.tsx';
-import CartList from './pages/CartList.tsx';
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import LoginPage from "./pages/LoginPage.tsx";
+import Order from "./pages/Order.tsx";
+import FoodDetail from "./pages/FoodDetail.tsx";
+import TmpDetail from "./pages/TmpDetail.tsx";
+import FakeDetail from "./pages/FakeDetail.tsx";
+import OrderList from "./pages/OrderList.tsx";
+import FakeOrder from "./pages/FakeOrder.tsx";
+import CartList from "./pages/CartList.tsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     children: [
       {
@@ -22,11 +22,11 @@ const router = createBrowserRouter([
         element: <Home />,
       },
       {
-        path: 'login',
+        path: "login",
         element: <LoginPage />,
       },
       {
-        path: 'stock/:storeName/:tableNumber',
+        path: "stock/:storeName/:tableNumber",
         element: (
           <div className="max-w-lg mx-auto">
             <Order />
@@ -34,29 +34,29 @@ const router = createBrowserRouter([
         ),
       },
       {
-        path: 'store/:storeName/:stock_id',
+        path: "store/:storeName/:stock_id",
         element: <FoodDetail />,
       },
       {
-        path: 'stock_list/:storeName/:tableNumber',
+        path: "stock_list/:storeName/:tableNumber",
         element: <OrderList />,
       },
       {
-        path: 'cart_list/:storeName/:tableNumber',
+        path: "cart_list/:storeName/:tableNumber",
         element: <CartList />,
       },
-      {
-        path: 'manage/order',
-        element: <TmpDetail />,
-      },
-      {
-        path: 'fake/order',
-        element: <FakeOrder />,
-      },
+      // {
+      //   path: 'manage/order',
+      //   element: <TmpDetail />,
+      // },
+      // {
+      //   path: 'fake/order',
+      //   element: <FakeOrder />,
+      // },
     ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />,
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <RouterProvider router={router} />
 );
--- src/pages/TmpDetail.tsx
+++ src/pages/TmpDetail.tsx
@@ -1,124 +1,124 @@
-import React, { useState } from 'react';
-import Header from '../components/Header';
-import { addStocks, STORE_CODE } from '../api/stocks';
-import { generateStockId } from '../util/uuid';
-import { Stock } from '../util/types';
+// import React, { useState } from 'react';
+// import Header from '../components/Header';
+// import { addStocks, STORE_CODE } from '../api/stocks';
+// import { generateStockId } from '../util/uuid';
+// import { Stock } from '../util/types';
 
-function TmpDetail() {
-  const [stockData, setStockData] = useState<Partial<Stock>>({
-    store_code: STORE_CODE,
-    stock_id: generateStockId(),
-    stock_option: {},
-    stock_image: [],
-  });
+// function TmpDetail() {
+//   const [stockData, setStockData] = useState<Partial<Stock>>({
+//     store_code: STORE_CODE,
+//     stock_id: generateStockId(),
+//     stock_option: {},
+//     stock_image: [],
+//   });
 
-  const [optionKeys, setOptionKeys] = useState<string[]>([]);
-  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+//   const [optionKeys, setOptionKeys] = useState<string[]>([]);
+//   const [selectedImage, setSelectedImage] = useState<File | null>(null);
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setStockData((prev) => ({ ...prev, [name]: value }));
-  };
+//   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     const { name, value } = e.target;
+//     setStockData((prev) => ({ ...prev, [name]: value }));
+//   };
 
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      setSelectedImage(e.target.files[0]);
-    }
-  };
+//   const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+//     if (e.target.files && e.target.files.length > 0) {
+//       setSelectedImage(e.target.files[0]);
+//     }
+//   };
 
-  const handleOptionChange = (key: string) => {
-    setStockData((prev) => ({
-      ...prev,
-      stock_option: {
-        ...prev.stock_option,
-        [key]: 0,
-      },
-    }));
-  };
+//   const handleOptionChange = (key: string) => {
+//     setStockData((prev) => ({
+//       ...prev,
+//       stock_option: {
+//         ...prev.stock_option,
+//         [key]: 0,
+//       },
+//     }));
+//   };
 
-  const handleAddOptionKey = () => {
-    setOptionKeys((prev) => [...prev, '']);
-  };
+//   const handleAddOptionKey = () => {
+//     setOptionKeys((prev) => [...prev, '']);
+//   };
 
-  const handleOptionKeyChange = (index: number, value: string) => {
-    const newOptionKeys = [...optionKeys];
-    newOptionKeys[index] = value;
-    setOptionKeys(newOptionKeys);
-    handleOptionChange(value);
-  };
+//   const handleOptionKeyChange = (index: number, value: string) => {
+//     const newOptionKeys = [...optionKeys];
+//     newOptionKeys[index] = value;
+//     setOptionKeys(newOptionKeys);
+//     handleOptionChange(value);
+//   };
 
-  const handleSubmit = async () => {
-    if (stockData) {
-      const formData = new FormData();
-      Object.keys(stockData).forEach((key) => {
-        formData.append(key, stockData[key]);
-      });
-      if (selectedImage) {
-        formData.append('stock_image', selectedImage);
-      }
+//   const handleSubmit = async () => {
+//     if (stockData) {
+//       const formData = new FormData();
+//       Object.keys(stockData).forEach((key) => {
+//         formData.append(key, stockData[key]);
+//       });
+//       if (selectedImage) {
+//         formData.append('stock_image', selectedImage);
+//       }
 
-      const success = await addStocks(formData); // Assuming addStocks can handle FormData
-      if (success) {
-        alert('Stock added successfully!');
-      } else {
-        alert('Error adding stock!');
-      }
-    }
-  };
+//       const success = await addStocks(formData); // Assuming addStocks can handle FormData
+//       if (success) {
+//         alert('Stock added successfully!');
+//       } else {
+//         alert('Error adding stock!');
+//       }
+//     }
+//   };
 
-  return (
-    <section>
-      <Header title="Add Stock" />
-      <div className="form-container">
-        <input
-          type="text"
-          name="stock_name"
-          placeholder="Stock Name"
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="stock_price"
-          placeholder="Stock Price"
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="stock_description"
-          placeholder="Description"
-          onChange={handleInputChange}
-        />
-        <input
-          type="text"
-          name="stock_category"
-          placeholder="Stock Category"
-          onChange={handleInputChange}
-        />
-        {optionKeys.map((key, index) => (
-          <input
-            key={index}
-            type="text"
-            value={key}
-            placeholder="Option Key"
-            onChange={(e) => handleOptionKeyChange(index, e.target.value)}
-          />
-        ))}
-        <input type="file" onChange={handleImageChange} />
-        <button
-          className="w-full p-2 bg-primary my-4 text-center text-white"
-          onClick={handleAddOptionKey}
-        >
-          Add Option Key
-        </button>
-        <button
-          className="w-full p-2 bg-primary my-4 text-center text-white"
-          onClick={handleSubmit}
-        >
-          ADD STOCK
-        </button>
-      </div>
-    </section>
-  );
-}
+//   return (
+//     <section>
+//       <Header title="Add Stock" />
+//       <div className="form-container">
+//         <input
+//           type="text"
+//           name="stock_name"
+//           placeholder="Stock Name"
+//           onChange={handleInputChange}
+//         />
+//         <input
+//           type="text"
+//           name="stock_price"
+//           placeholder="Stock Price"
+//           onChange={handleInputChange}
+//         />
+//         <input
+//           type="text"
+//           name="stock_description"
+//           placeholder="Description"
+//           onChange={handleInputChange}
+//         />
+//         <input
+//           type="text"
+//           name="stock_category"
+//           placeholder="Stock Category"
+//           onChange={handleInputChange}
+//         />
+//         {optionKeys.map((key, index) => (
+//           <input
+//             key={index}
+//             type="text"
+//             value={key}
+//             placeholder="Option Key"
+//             onChange={(e) => handleOptionKeyChange(index, e.target.value)}
+//           />
+//         ))}
+//         <input type="file" onChange={handleImageChange} />
+//         <button
+//           className="w-full p-2 bg-primary my-4 text-center text-white"
+//           onClick={handleAddOptionKey}
+//         >
+//           Add Option Key
+//         </button>
+//         <button
+//           className="w-full p-2 bg-primary my-4 text-center text-white"
+//           onClick={handleSubmit}
+//         >
+//           ADD STOCK
+//         </button>
+//       </div>
+//     </section>
+//   );
+// }
 
-export default TmpDetail;
+// export default TmpDetail;
