c2da673d7c49d47db0fa9581a243f260db40ad1f
bishoe01 <bishoe01@gmail.com>
2023-09-16 19:03:03 +0900

    정리

---
+++ src/components/MenuCard.tsx
@@ -0,0 +1,36 @@
+import { Link } from 'react-router-dom';
+import { Stock } from '../util/types';
+
+interface MenuCardProps {
+  menu: Stock;
+  isFirst: boolean;
+  store_name: string;
+}
+
+function MenuCard({ menu, isFirst, store_name }: MenuCardProps) {
+  return (
+    <Link
+      to={`/store/${store_name}/${menu.stock_id}`}
+      style={{ borderBottom: '0.4px solid #E5E5E5' }}
+      className={`flex items-center justify-between p-5 border-LineGray ${
+        isFirst ? 'border-t-[0.4px]' : ''
+      }`}
+    >
+      <div className="flex flex-col items-start gap-[9px]">
+        <h1 className="medium">{menu.stock_name}</h1>
+        <p className="line-clamp-2 w-3/4 text-[14px] text-grayLight text-start light">
+          {menu.stock_description} Lorem, ipsum dolor sit amet consectetur
+          adipisicing elit. Quidem, nisi?
+        </p>
+        <span className="medium">{menu.stock_price.toLocaleString()}원</span>
+      </div>
+      <img
+        className="w-[80px] h-[80px] object-cover rounded-md"
+        src={menu.stock_image[0]}
+        alt={menu.stock_name}
+      />
+    </Link>
+  );
+}
+
+export default MenuCard;
